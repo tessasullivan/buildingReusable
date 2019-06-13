d3fdf7ef6e16bcaffdc9de0b9fbe04bb7030ec44
Student <example@example.com>
2019-06-13 15:49:23 -0700

    Added favorite component - currently not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FavoriteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
